a2305366dff532184b49b9206abb2c1499d7551c
abhisheknair10 <nairabs10@gmail.com>
2021-09-30 01:06:24 +0100

    Final Stage

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -28,20 +28,28 @@ document.getElementById('back-to-main').onclick = function() {
 document.getElementById('confirm-withdraw').onclick = function() {
     walletaddr = document.getElementById("wallet-address").value;
     transferamount = document.getElementById("transfer-amount").value;
-    var client = new HttpClient();
-    client.get('https://blackrhino-ce.com/withdraw/' + userName + '/' + secretHash + 
-    '/' + walletaddr + '/' + transferamount, function(response) {
-        if(response == "non-existent"){
-            alert("Something went wrong while verifying your account")
-        }
-        else if(response == "wrong-hash"){
-            alert("Something went wrong while verifying your account")
-        }
-        else if(response == "too-much-amount"){
-            alert("You do not have the requested amount of Bitcoin in your Black Rhino Wallet")
-        }
-        else if(response == "sent-to-wallet"){
-            alert("Funds have been sent to wallet")
-        }
-    });
+    if(transferamount > 0){
+        var client = new HttpClient();
+        client.get('https://blackrhino-ce.com/withdraw/' + userName + '/' + secretHash + 
+        '/' + walletaddr + '/' + transferamount, function(response) {
+            if(response == "non-existent"){
+                alert("Something went wrong while verifying your account")
+            }
+            else if(response == "wrong-hash"){
+                alert("Something went wrong while verifying your account")
+            }
+            else if(response == "too-much-amount"){
+                alert("You do not have the requested amount of Zcash in your Black Rhino Wallet")
+            }
+            else if(response == "sent-to-wallet"){
+                alert("Withdraw Request has been sent. It could take up to 24 hours till you see your funds in your wallet")
+            }
+            else if(response == "request-already-there"){
+                alert("Withdraw Request for this account already exists. Please send another withdraw request after the previous request has been approved")
+            }
+        });
+    }
+    else{
+        alert("Withdraw Amount has to be greater than 0")
+    }
 }
